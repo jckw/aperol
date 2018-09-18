7d37b7317f3cd77d19aca4dc2c9880403a7e4dfa
Jack Weatherilt <jackweatherilt@outlook.com>
2018-09-18 12:50:46 +0100

    Add extension creation to initial migration.

--- aperol/properties/migrations/0001_initial.py
+++ aperol/properties/migrations/0001_initial.py
@@ -5,6 +5,7 @@ import django.contrib.gis.db.models.fields
 from django.db import migrations, models
 import django.db.models.deletion
 import aperol.properties.models
+from django.contrib.postgres.operations import CreateExtension
 
 
 class Migration(migrations.Migration):
@@ -16,6 +17,7 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
+        CreateExtension('postgis'),
         migrations.CreateModel(
             name='AgencyReview',
             fields=[
